d3bdf5b49fa334e7f5dd2fabb333158eb2ee0b9c
ANITHARANGANATHAN <92741441+ANITHARANGANATHAN@users.noreply.github.com>
2022-03-08 21:20:57 +0530

    radiusofcircle.py

--- radiusofcircle.py
+++ radiusofcircle.py
@@ -1,3 +1,15 @@
+#radius of a circle from the user and computers area
+
+
 from math improt pi
 r=float(input("input the radius of cicle :"))
 print("the area of the cicle with radius "+ str(r)+"is:"+ str(pi*r**2))
+
+
+
+
+#to accept a filename from user and print the extension of that: 
+
+filename = input("input the filename: ")
+f_extns = filename.spilt(".")
+print ("the extension of the file is :"+ repr(f_extns[-1]))
