5f4976341a94aa28b16410349e52cd4fc5865292
ANITHARANGANATHAN <92741441+ANITHARANGANATHAN@users.noreply.github.com>
2022-03-08 21:34:11 +0530

    function.py

--- function.py
+++ function.py
@@ -1,9 +1,23 @@
-def most_frequent(string):
-  d = dict()
-  for key in string:
-    if key not in d:
-      d[key] = 1
-      else:
-d[key] +=1
-return d
-print most_frequent('aabbc')
+def most_frequent(s):
+    t=s.split()
+    delimiter= ''
+    s=delimiter.join(t)
+    l=list(s) #['j', 'a', 'n', 'k', 'o', 's', 'i', 'e', 'n', 'a', 'p', 'i', 'v', 'o']
+    f=[]
+    for i in l:
+        f.append(l.count(i)) # [1, 2, 2, 1, 2, 1, 2, 1, 2, 2, 1, 2, 1, 2]
+    tup=zip(f,l)
+    tup.sort(reverse=True)
+    res=[]
+    for freq,letter in tup:
+        if letter not in res:
+            res.append(letter)
+    print res # ['o', 'n', 'i', 'a', 'v', 's', 'p', 'k', 'j', 'e']
+
+
+def main():
+    s='janko sie na pivo'
+    most_frequent(s)
+
+if __name__ == '__main__':
+    main()
